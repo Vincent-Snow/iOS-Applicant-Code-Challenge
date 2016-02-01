e518f7c10f25585a8012d8e5a436003a109d05fe
Vince <vincent.van.snow@gmail.com>
2016-02-01 14:03:03 -0700

    completed assignment

--- DevMtn Code Challenge.playground/Pages/About Me.xcplaygroundpage/Contents.swift
+++ DevMtn Code Challenge.playground/Pages/About Me.xcplaygroundpage/Contents.swift
@@ -3,10 +3,13 @@
 
 Tell us a little bit about yourself using Swift variables. Start with your first name, your last name, your age, where you're from, and why you want to take the class.
 */
-
-
+let firstName = "Vincent"
+let lastName = "Snow"
+let age = 24
+let homeTown = "Fountain Valley, California"
+let classMotiv = "To learn how to develop iPhone apps"
 //: Create an array of Strings that holds a few of your hobbies.
-
+let hobbies = ["surfing", "racquetball", "skiing", "hiking",]
 
 /*: Programmatically combine the individual strings from your hobbies array into a single string with the hobbies separated by commas.
 
@@ -15,7 +18,7 @@ Tell us a little bit about yourself using Swift variables. Start with your first
 
 
 //: Using the variables you have created, write a programmatically generated sentence to introduce yourself. Use only one print() statement.
-
+print("Hi my name is \(firstName) \(lastName). I am \(age) years old. I am from \(homeTown), and I am taking this class \(classMotiv)")
 
 //: [Previous](@previous)
 //: [Next](@next)
--- DevMtn Code Challenge.playground/Pages/Basic Math.xcplaygroundpage/Contents.swift
+++ DevMtn Code Challenge.playground/Pages/Basic Math.xcplaygroundpage/Contents.swift
@@ -6,14 +6,33 @@ Demonstrate basic knowledge of functions that take parameters and return results
 //: Write a function that returns the sum of two numbers
 
 
-//: Write a function that returns the product of two numbers
+func add(x: Int, y: Int) -> Int {
+    return x + y
+}
 
+//: Write a function that returns the product of two numbers
+func times(x1: Int, y1: Int) -> Int {
+    return x1 * y1
+}
 
 //: Write a function that returns the average (mean) of an array of numbers
 
 
+func average(numbers: [Int]) -> Double {
+    var sum = 0
+    
+    for number in numbers {
+        
+        sum += number
+        
+    }
+    let mean : Double = Double(sum) / Double(numbers.count)
+    return mean
+}
+
 //: Demo the use of your functions here:
-
-
+print(add(5, y: 6))
+print(times(3, y1: 2))
+print(average([4, 3, 5, 6, 7, 2, 2, 2, 2]))
 //: [Previous](@previous)
 //: [Next](@next)
